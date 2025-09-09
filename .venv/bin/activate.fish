989751552112ca3466397b2f51c0cfeaf44733dc
Jeandre Coetzee <Jeandre@Alvirnos-MacBook-Air.local>
2025-09-09 15:48:59 +0200

    feat

--- .venv/bin/activate.fish
+++ .venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/Jeandre/Desktop/ZAPPIES CUSTOMERS/IPIC PLAY & PARTY/IPIC PROJECTS/.venv"
+set -gx VIRTUAL_ENV "/Users/Jeandre/Desktop/ZAPPIES CUSTOMERS/IPIC PLAY & PARTY/IPIC PROJECTS/IPIC_Play_and_Party_Dashboard/.venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
